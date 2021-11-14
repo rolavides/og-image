bcad25d25a6adf01d3a4c621b50caf6308b2a292
rolavides <5779571+rolavides@users.noreply.github.com>
2021-11-13 22:37:04 -0800

    Vercel complains of unused variables

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -12,14 +12,10 @@ const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('b
 const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
-    let background = 'white';
     let foreground = 'black';
-    let radial = 'lightgray';
 
     if (theme === 'dark') {
-        background = 'black';
         foreground = 'white';
-        radial = 'dimgray';
     }
     return `
     @font-face {
